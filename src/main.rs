465696c0c66cca3621482b32ef9025d5728871a7
Justin Kur <Justink97@hotmail.com>
2022-01-24 21:59:54 -0500

    Async play

--- src/main.rs
+++ src/main.rs
@@ -12,15 +12,20 @@ use std::collections::HashMap;
 use std::io::Read;
 use std::{env, time};
 use topaz_tak::board::{Board5, Board6, Board7};
+use topaz_tak::generate_all_moves;
 use topaz_tak::search::proof::TinueSearch;
 use topaz_tak::{Color, GameMove, TakBoard, TakGame};
 
 lazy_static! {
     static ref PTN_META: Regex = Regex::new(r#"\[(?P<Key>.*?) "(?P<Value>.*?)"\]"#).unwrap();
-    static ref PTN_MOVE: Regex = Regex::new(r#"([SC1-8]?[A-Ha-h]\d[+-<>]?\d*)"#).unwrap();
+    static ref PTN_MOVE: Regex = Regex::new(r#"([SCsc1-8]?[A-Ha-h]\d[+-<>]?\d*)"#).unwrap();
 }
 
+mod play;
+
 const TAK_TALK: ServerId = ServerId(176389490762448897);
+const NODE_LIMIT: usize = 100_000;
+static TOPAZ: &'static str = "topazbot";
 
 fn main() {
     dotenv().ok();
@@ -41,8 +46,21 @@ fn main() {
         .iter()
         .filter_map(|c| AsyncChannel::try_new(c))
         .collect();
-    for m in matches {
-        println!("{:?}", m);
+    // TODO check new challenge creation while bot is running
+    for m in matches.iter() {
+        let channel_id = m.id;
+        let messages = discord
+            .get_messages(m.id, discord::GetMessages::MostRecent, Some(40))
+            .unwrap();
+        let game =
+            play::search_room(m.id, &discord, true, Some(messages)).expect("Search room failed!");
+        if let (Some(board), Some(topaz_turn)) = (game.board, game.topaz_turn) {
+            if topaz_turn {
+                play::play_async_move(board, channel_id, &discord).expect("Failed to send message");
+            }
+        } else {
+            println!("Could not fully determine game state {}!", channel_id);
+        }
     }
     loop {
         match connection.recv_event() {
@@ -52,7 +70,42 @@ fn main() {
                     if let Err(e) = handle_tinue_req(&discord, &message) {
                         println!("{}", e);
                     }
+                } else if matches
+                    .iter()
+                    .find(|chan| chan.id == message.channel_id)
+                    .is_some()
+                {
+                    if message.content.starts_with("Your turn ") {
+                        if let Ok(game) =
+                            play::search_room(message.channel_id, &discord, true, None)
+                        {
+                            if let (Some(board), Some(topaz_turn)) = (game.board, game.topaz_turn) {
+                                if topaz_turn {
+                                    play::play_async_move(board, message.channel_id, &discord)
+                                        .expect("Failed to send message");
+                                }
+                            } else {
+                                println!(
+                                    "Could not fully determine game state {}!",
+                                    message.channel_id
+                                );
+                            }
+                        }
+                    } else if message.content.starts_with("!topaz position") {
+                        match play::search_room(message.channel_id, &discord, false, None) {
+                            Ok(game) => {
+                                let s = format!("This is the position, right? \n{:?}", game.board);
+                                discord
+                                    .send_message(message.channel_id, &s, "", false)
+                                    .expect("Failed to send message!");
+                            }
+                            Err(e) => {
+                                println!("Position get failed: {:?}", e);
+                            }
+                        }
+                    }
                 }
+                // Todo respond while still logged in
             }
             Ok(_) => {}
             Err(discord::Error::Closed(code, body)) => {
@@ -79,11 +132,15 @@ impl AsyncChannel {
         if iter.next().is_some() {
             None
         } else {
-            Some(Self {
-                id: channel.id,
-                player1: p1.to_string(),
-                player2: p2.to_string(),
-            })
+            if p1 == TOPAZ || p2 == TOPAZ {
+                Some(Self {
+                    id: channel.id,
+                    player1: p1.to_string(),
+                    player2: p2.to_string(),
+                })
+            } else {
+                None
+            }
         }
     }
 }
@@ -104,36 +161,40 @@ impl<'a> TinueRequest<'a> {
             .split_whitespace()
             .nth(1)
             .ok_or_else(|| anyhow!("Bad query request"))?;
-        if let Ok(game_id) = details.parse::<u32>() {
-            // Assume it is a playtak id
-            let mut buffer = String::new();
-            let ssl = NativeTlsClient::new().unwrap();
-            let connector = HttpsConnector::new(ssl);
-            let client = Client::with_connector(connector);
-            let url = format!("https://www.playtak.com/games/{}/view", game_id);
-            let mut res = client.get(&url).send()?;
-            res.read_to_string(&mut buffer)?;
-            Ok(buffer)
+        get_ptn_string(details)
+    }
+}
+
+fn get_ptn_string(details: &str) -> Result<String> {
+    if let Ok(game_id) = details.parse::<u32>() {
+        // Assume it is a playtak id
+        let mut buffer = String::new();
+        let ssl = NativeTlsClient::new().unwrap();
+        let connector = HttpsConnector::new(ssl);
+        let client = Client::with_connector(connector);
+        let url = format!("https://www.playtak.com/games/{}/view", game_id);
+        let mut res = client.get(&url).send()?;
+        res.read_to_string(&mut buffer)?;
+        Ok(buffer)
+    } else {
+        // See if it is a ptn.ninja link
+        if let Some(substr) = details.split("ptn.ninja/").nth(1) {
+            let part = substr
+                .split("&name")
+                .next()
+                .ok_or_else(|| anyhow!("Bad ptn ninja link!"))?;
+            // println!("{}", part);
+            let decompressed = decompress_uri(&str_to_u32_vec(part))
+                .ok_or_else(|| anyhow!("Bad ptn ninja game string"))?;
+            Ok(decompressed)
         } else {
-            // See if it is a ptn.ninja link
-            if let Some(substr) = details.split("ptn.ninja/").nth(1) {
-                let part = substr
-                    .split("&name")
-                    .next()
-                    .ok_or_else(|| anyhow!("Bad ptn ninja link!"))?;
-                // println!("{}", part);
-                let decompressed = decompress_uri(&str_to_u32_vec(part))
-                    .ok_or_else(|| anyhow!("Bad ptn ninja game string"))?;
-                Ok(decompressed)
-            } else {
-                Err(anyhow!("Unknown query format!"))
-            }
+            Err(anyhow!("Unknown query format!"))
         }
     }
 }
 
 fn handle_tinue_req(discord: &Discord, message: &Message) -> Result<()> {
-    let req = TinueRequest::new(&message.content, &message.author.name);
+    let req = TinueRequest::new(&message.author.name, &message.content);
     let start_time = time::Instant::now();
     let ptn = req.get_ptn_string()?;
     let game = parse_game(&ptn).ok_or_else(|| anyhow!("Unable to parse game"))?;
@@ -143,36 +204,35 @@ fn handle_tinue_req(discord: &Discord, message: &Message) -> Result<()> {
         TakGame::Standard7(board) => find_all_tinue(board, &game.1),
         _ => todo!(),
     };
-    // This is kind of ugly, but it's fine for the moment
-    let (tinue, timeout): (Vec<_>, Vec<_>) = tinue_plies.clone().into_iter().partition(|t| {
-        if let TinueStatus::Tinue(_) = t {
-            true
-        } else {
-            false
+
+    let mut tinue = Vec::new();
+    let mut road = Vec::new();
+    let mut timeout = Vec::new();
+    for ply in tinue_plies.into_iter() {
+        match ply {
+            TinueStatus::Tinue(_) => tinue.push(ply.to_string()),
+            TinueStatus::Road(_) => road.push(ply.to_string()),
+            TinueStatus::Timeout(_) => timeout.push(ply.to_string()),
         }
-    });
-    let tinue: Vec<_> = tinue.into_iter().map(|x| x.to_string()).collect();
-    let timeout: Vec<_> = timeout.into_iter().map(|x| x.to_string()).collect();
-    let tinue_s = if tinue.len() == 0 {
-        "None".to_string()
-    } else {
-        tinue.join(", ")
-    };
-    let timeout_s = if timeout.len() == 0 {
-        "None".to_string()
-    } else {
-        timeout.join(", ")
+    }
+    let printable = |vec: Vec<String>| {
+        if vec.len() == 0 {
+            "None".to_string()
+        } else {
+            vec.join(", ")
+        }
     };
     let duration = time::Instant::now().duration_since(start_time);
 
     discord.send_message(
         message.channel_id,
         &format!(
-            "Sure thing, {}! Completed in {} ms.\nTinue: {}\nTimeout: {}",
+            "Sure thing, {}! Completed in {} ms.\nTinue: {}\nRoad: {}\nTimeout: {}",
             message.author.name,
             duration.as_millis(),
-            tinue_s,
-            timeout_s
+            printable(tinue),
+            printable(road),
+            printable(timeout),
         ),
         "",
         false,
@@ -183,6 +243,7 @@ fn handle_tinue_req(discord: &Discord, message: &Message) -> Result<()> {
 #[derive(Debug, Clone, Copy)]
 enum TinueStatus {
     Tinue(usize),
+    Road(usize),
     Timeout(usize),
 }
 
@@ -190,6 +251,7 @@ impl std::fmt::Display for TinueStatus {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
         let ply = match self {
             TinueStatus::Tinue(x) => x,
+            TinueStatus::Road(x) => x,
             TinueStatus::Timeout(x) => x,
         };
         let color = if ply % 2 == 0 { "W" } else { "B" };
@@ -209,11 +271,17 @@ fn find_all_tinue<T: TakBoard + std::fmt::Debug>(
             continue;
         }
         let s = move_s(idx);
-        let mut search = TinueSearch::new(board).limit(100_000).quiet();
-        if let Some(b) = search.is_tinue() {
-            if b {
-                vec.push(TinueStatus::Tinue(idx + 2));
-                println!("{}: Tinue", s);
+        let mut search = TinueSearch::new(board).limit(NODE_LIMIT).quiet();
+        if let Some(is_tinue) = search.is_tinue() {
+            if is_tinue {
+                let road_move = find_road_move(&mut search.board);
+                if road_move.is_some() {
+                    vec.push(TinueStatus::Road(idx + 2));
+                    println!("{}: Road", s);
+                } else {
+                    vec.push(TinueStatus::Tinue(idx + 2));
+                    println!("{}: Tinue", s);
+                }
             } else {
                 println!("{}: Not Tinue", s);
             }
@@ -226,6 +294,19 @@ fn find_all_tinue<T: TakBoard + std::fmt::Debug>(
         board.do_move(*mv);
     }
     vec
+}
+
+fn find_road_move<B: TakBoard>(board: &mut B) -> Option<GameMove> {
+    let mut moves = Vec::new();
+    let side = board.side_to_move();
+    generate_all_moves(board, &mut moves);
+    let road = moves.into_iter().find(|mv| {
+        let rev = board.do_move(*mv);
+        let road = board.road(side);
+        board.reverse_move(rev);
+        road
+    });
+    road
 }
 
 fn move_s(idx: usize) -> String {
@@ -241,6 +322,41 @@ fn move_s(idx: usize) -> String {
     format!("{}. {}", idx / 2, color)
 }
 
+fn parse_move(mv_str: &str, size: usize, color: Color) -> Option<GameMove> {
+    let mut iter = mv_str.chars();
+    let first = iter.next()?;
+    let mv = if first == 'S' || first == 's' {
+        let mut s = String::new();
+        // Lowercase everything besides this first S
+        s.push(first.to_ascii_uppercase());
+        while let Some(c) = iter.next() {
+            s.push(c.to_ascii_lowercase());
+        }
+        s
+    } else if first == 'C' || first == 'c' {
+        let mut s = String::new();
+        // We need to find out if this C is a capstone or a bad square indicator
+        let second = iter.next()?;
+        if second.is_ascii_alphabetic() {
+            // The previous must have been a capstone, make it capitalized
+            s.push(first.to_ascii_uppercase());
+            s.push(second.to_ascii_lowercase());
+        } else {
+            // The second must be a number, meaning this should be lowercase c
+            s.push(first.to_ascii_lowercase());
+            s.push(second);
+        }
+        while let Some(c) = iter.next() {
+            s.push(c.to_ascii_lowercase());
+        }
+        s
+    } else {
+        mv_str.to_ascii_lowercase()
+    };
+    let mv = GameMove::try_from_ptn_m(&mv, size, color)?;
+    Some(mv)
+}
+
 fn parse_game(full_ptn: &str) -> Option<(TakGame, Vec<GameMove>)> {
     let mut meta = HashMap::new();
     let mut moves = Vec::new();
@@ -250,38 +366,7 @@ fn parse_game(full_ptn: &str) -> Option<(TakGame, Vec<GameMove>)> {
     }
     let size = meta.get("Size")?.parse().ok()?;
     for m in PTN_MOVE.captures_iter(&full_ptn.split("{").next()?) {
-        let mv_str = &m[0];
-        let mut iter = mv_str.chars();
-        let first = iter.next()?;
-        let mv = if first == 'S' {
-            let mut s = String::new();
-            // Lowercase everything besides this first S
-            s.push(first);
-            while let Some(c) = iter.next() {
-                s.push(c.to_ascii_lowercase());
-            }
-            s
-        } else if first == 'C' {
-            let mut s = String::new();
-            // We need to find out if this C is a capstone or a bad square indicator
-            let second = iter.next()?;
-            if second.is_ascii_alphabetic() {
-                // The previous must have been a capstone, leave it capitalized
-                s.push(first);
-                s.push(second.to_ascii_lowercase());
-            } else {
-                // The second must be a number, meaning this should be lowercase c
-                s.push(first.to_ascii_lowercase());
-                s.push(second);
-            }
-            while let Some(c) = iter.next() {
-                s.push(c.to_ascii_lowercase());
-            }
-            s
-        } else {
-            m[0].to_ascii_lowercase()
-        };
-        let mv = GameMove::try_from_ptn_m(&mv, size, color)?;
+        let mv = parse_move(&m[0], Board6::SIZE, color)?;
         moves.push(mv);
         color = !color;
     }
@@ -305,6 +390,10 @@ fn parse_game(full_ptn: &str) -> Option<(TakGame, Vec<GameMove>)> {
 #[cfg(test)]
 mod test {
     use super::*;
+    // #[test]
+    // fn playtak() {
+    //     let g1 = ""
+    // }
     #[test]
     fn ptn_ninja() {
         let s1 = concat!(
@@ -319,10 +408,31 @@ mod test {
             "AaSUzJSSD0vIL8gHoMoKF01I9irJygyXKs1JVi1Lza7NzMiqCStq6azJ7UgDZMosbhmEkYII8NalncIA&name=CIQwlgNgngBACgV", 
             "wF5IgUxgYgIwGYBsAdLjABQBCaA5mAHa1oBOAlEA&showPTN=false&theme=MYGwhgzhCWxA"
         );
-        for s in [s1, s2].iter() {
+        let s3 = concat!(
+            "!tinue https://ptn.ninja/NoEQhgLgpgBARAJgAwIQOiQRjZgzHAXWABUBLAW1jkwA4AuXXOgVgHZDgBlU6eACwgQADgGc6AenE", 
+            "BzHnwCuAIzQBjAPblx88mAB2kcRDABrAEKqIHAAoAbMAE8oAJ0zwwAKwAepK7YeOE8DoAjk6qOhzcAF5UAGwcANLqpPAIHADyQlA6pD",
+            "pS8CIA7mBCHOC8iCjoWGgIACwcZJTweHRItQw0HABKUCJy1hDwSAC0AGKEMACCLmAxMADCzDAKS-O1y+vzuDDKASbbCgGT2-MAJtsmc",
+            "6cB83MKLvNQAVDbAKIIADw7CADUMJy7GAAMxOvxg5y+CGuf0BIACpxcAJcTwWmAAfMD1qMllAlq8XKd1q91rjhjB9mSEMpcH8oTSYGAl",
+            "pwQAdMLSETCXCAZsw-rhrh9MAEqbVhpgXPzahjcLsfkKYCyvspRTBmFBaoL1ucFSrrjrwWCQCr9gqApxCQqloSybiYLVzj9UDBcIc-vc",
+            "YCJAU8vhydrgvrVlWTJqtPv9YQTcGTzdswEco3brminSy-nrJWKAkCJUmDXantLTswvsTwesYkDfpglpWYCNRkA",
+        );
+        let s4 = "!tinue https://ptn.ninja/NoEQhgLgpgBARAJgAwIQOiQRjQgzHAXWABUBLAW1jkwE4AuAFgDY6EHDgBlU6eACwgQADgGc6AenEBzHnwCuAIzQBjAPblx88mAB2kcRDABrAEKqIHAAoAbMAE8oAJ0zwAslD6OA4qWvWRVrYOjghuumDEHNwAXlRMHADS6qTwCBwA8kJQOqQ6UvAiAO5gQoQwAIIuCi4mocqhJrgwAMIKDC0uACahIFUMALQVoVChAKIuAGYuAGKhCAq4g+UIg1MAPC1TMJxgobMbu4PTmBudTaNNUO0g7Z0ArDCjD-eDnMrtnFBMLQwAfDALIYA9rNJpgJoIKCYADUAIe5XaYAeuE6DDWmFCqJgDBGa1QMGUDxAD2U3xA30+oWafxgZ1h1JguJaCH+uDAmF+GJgCG6G1wCgQ0IQLk4AqAA";
+        for s in [s1, s2, s3, s4].iter() {
             let t = TinueRequest::new("", &s);
             let ptn = t.get_ptn_string().unwrap();
+            if s == &s4 {
+                println!("{}", ptn);
+            }
             let parsed = parse_game(&ptn);
+            if s == &s3 {
+                if let Some((_, ref moves)) = parsed {
+                    let count = moves
+                        .iter()
+                        .filter(|m| &m.to_ptn::<Board6>() == "Sc2")
+                        .count();
+                    assert_eq!(count, 3);
+                }
+            }
             assert!(parsed.is_some());
         }
     }
---
+++ src/play.rs
@@ -0,0 +1,167 @@
+use std::time::Duration;
+
+use topaz_tak::{
+    eval::Weights6,
+    search::{search, SearchInfo},
+    Position,
+};
+
+use super::*;
+use discord::model::UserId;
+
+const TOPAZ_ID: UserId = UserId(211376698778714123); // Real Topaz
+                                                     // const TOPAZ_ID: UserId = UserId(166005222794199042);
+const TAK_BOT_ID: UserId = UserId(793658103668539424);
+
+static LINK_START: &'static str = "<https://ptn.ninja/";
+
+pub struct AsyncGameState {
+    pub topaz_turn: Option<bool>,
+    pub board: Option<Board6>,
+}
+
+impl AsyncGameState {
+    fn new(topaz_turn: Option<bool>, board: Option<Board6>) -> Self {
+        Self { topaz_turn, board }
+    }
+}
+
+pub fn search_room(
+    channel: ChannelId,
+    discord: &Discord,
+    only_topaz: bool,
+    use_messages: Option<Vec<Message>>,
+) -> Result<AsyncGameState> {
+    let messages = if let Some(m) = use_messages {
+        m
+    } else {
+        discord.get_messages(channel, discord::GetMessages::MostRecent, Some(10))?
+    };
+    // Search most recent first
+    let topaz_turn = my_turn(&messages);
+    if only_topaz && topaz_turn.is_none() {
+        return Err(anyhow!("Could not determine which player's turn it is"));
+    }
+    let game_link = find_link(&messages);
+    let board = if let Some(link) = game_link {
+        let ptn = super::get_ptn_string(&link)?;
+        let game = super::parse_game(&ptn).ok_or_else(|| anyhow!("Could not read ptn!"))?;
+        match game {
+            (TakGame::Standard6(mut board), moves) => {
+                for m in moves {
+                    board.do_move(m);
+                }
+                let mut extra_moves = Vec::new();
+                for message in messages.iter() {
+                    if message.content.starts_with(LINK_START) {
+                        break;
+                    }
+                    if super::parse_move(&message.content, Board6::SIZE, board.side_to_move())
+                        .is_some()
+                    {
+                        extra_moves.push(&message.content);
+                    }
+                }
+                for mv in extra_moves.into_iter().rev() {
+                    let m = super::parse_move(&mv, Board6::SIZE, board.side_to_move()).unwrap();
+                    board.do_move(m);
+                }
+                board
+            }
+            _ => {
+                return Err(anyhow!("Unsupported game size!"));
+            }
+        }
+    } else {
+        discord.send_message(channel, "!tak link", "", false)?;
+        std::thread::sleep(Duration::from_secs(5));
+        let messages = discord.get_messages(channel, discord::GetMessages::MostRecent, Some(5))?;
+        let link =
+            find_link(&messages).ok_or_else(|| anyhow!("Takbot did not respond with link!"))?;
+        let ptn = super::get_ptn_string(&link)?;
+        let game = super::parse_game(&ptn).ok_or_else(|| anyhow!("Could not read ptn!"))?;
+        match game {
+            (TakGame::Standard6(mut board), moves) => {
+                for m in moves {
+                    board.do_move(m);
+                }
+                board
+            }
+            _ => {
+                return Err(anyhow!("Unsupported game size!"));
+            }
+        }
+    };
+    Ok(AsyncGameState::new(topaz_turn, Some(board)))
+}
+
+fn find_link(messages: &[Message]) -> Option<String> {
+    for message in messages.iter() {
+        println!("{:?}: {}", message.author, message.content);
+        if message.content.starts_with("!tak undo") {
+            break;
+        } else if message.content.starts_with("!tak rematch") {
+            break;
+        } else if message.content.starts_with("Invalid move.") {
+            break;
+        } else if message.author.id == TAK_BOT_ID && message.content.starts_with(LINK_START) {
+            let link = message
+                .content
+                .chars()
+                .filter(|&c| c != '<' && c != '>')
+                .collect();
+            return Some(link);
+        }
+    }
+    None
+}
+
+pub fn my_turn(messages: &[Message]) -> Option<bool> {
+    // let messages = discord.get_messages(channel, discord::GetMessages::MostRecent, Some(10))?;
+    for message in messages.iter() {
+        if message.author.id == TAK_BOT_ID && message.content.starts_with("Your turn ") {
+            if let Some(user) = message.mentions.iter().next() {
+                if user.id == TOPAZ_ID {
+                    return Some(true);
+                } else {
+                    return Some(false);
+                }
+            } else {
+                return None;
+            }
+        }
+    }
+    None
+}
+
+pub fn play_async_move(mut board: Board6, channel: ChannelId, discord: &Discord) -> Result<()> {
+    let mut tinue_search = TinueSearch::new(board).limit(NODE_LIMIT * 5).quiet();
+    let best_move = if Some(true) == tinue_search.is_tinue() {
+        let pv_move = tinue_search.principal_variation().into_iter().next();
+        if let Some(mv) = pv_move {
+            mv.to_ptn::<Board6>()
+        } else {
+            // Maybe it's just one ply?
+            let road_move = find_road_move(&mut tinue_search.board);
+            if let Some(mv) = road_move {
+                mv.to_ptn::<Board6>()
+            } else {
+                return Err(anyhow!("Failed getting tinue pv search / road move!"));
+            }
+        }
+    } else {
+        board = tinue_search.board;
+        let mut info = SearchInfo::new(6, 1000000).max_time(30);
+        let mut eval = Weights6::default();
+        if board.move_num() <= 6 {
+            eval.add_noise();
+        }
+        // let message = { 0 };
+        search(&mut board, &eval, &mut info)
+            .and_then(|x| x.best_move())
+            .ok_or_else(|| anyhow!("No best move from game search!"))?
+    };
+    std::thread::sleep(Duration::from_secs(5));
+    discord.send_message(channel, &best_move, "", false)?;
+    Ok(())
+}
